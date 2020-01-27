507b15f0470fe23b9a0deea5734f2b6df4dfd13d
J.Strub <jstrub90@gmail.com>
2020-01-26 20:04:54 -0500

    added reactstrap and ivip image

---
+++ i-vip/client/src/components/nav/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const Nav = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">IVIP</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/home/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="profile">Profile</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>iVIP</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Nav;
